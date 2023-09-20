2a179d8a0b0933eee3d055fe564ab5fc8d8deef4
yozura <yozura43@naver.com>
2023-09-20 22:36:32 +0900

    update: collisionupdate

--- mc/mc.cpp
+++ mc/mc.cpp
@@ -43,31 +43,31 @@ using namespace filament;
 using utils::Entity;
 using utils::EntityManager;
 
-static const int   gCircleCount = 20;
-static const int   gSegments    = 100;
-static const float gRadius      = 0.5f;
-static const float gSpeed       = 100.0f;
-
 struct Vertex {
     filament::math::float2 mPos;
     uint32_t               mColor;
 };
 
+const int   gCircleCount = 20;
+const int   gSegments    = 100;
+const float gSpeed       = 20.0f;
+Vertex      gVertices[gSegments + 1];
+uint16_t    gIndicies[gSegments * 3];
+double      gLastTime    = 0.0;
+
 struct Circle {
     filament::math::float2 mCenter;
     filament::math::float2 mVelocity;
-    Vertex                 mVertices[gSegments + 1];
-    uint16_t               mIndicies[gSegments * 3];
     float                  mRadius;
-    float                  mMass;
-    uint32_t               mColor;
+};
+
+struct Wall {
+    float up, down, left, right;
 };
 
 struct Renderable_t
 {
     Material*     mMaterial;
-    VertexBuffer* mVertexBuffer;
-    IndexBuffer*  mIndexBuffer;
     Circle        mCircle;
     Entity        mRenderable;
 };
@@ -77,9 +77,12 @@ struct App {
     Camera*       mCamera;
     Skybox*       mSkybox;
     Entity        mCameraObj;
+    VertexBuffer* mVertexBuffer;
+    IndexBuffer*  mIndexBuffer;
     
     Renderable_t  mRenderableArr[gCircleCount];
     uint32_t      mCurrentCount;
+    Wall          mWall;
 };
 
 float randf() { return (float)rand() / (float)RAND_MAX; }
@@ -91,27 +94,21 @@ float dist(filament::math::float2 left, filament::math::float2 right)
     return sqrt((right.x - left.x) * (right.x - left.x) + (right.y - left.y) * (right.y - left.y));
 }
 
-void Collision(Circle& a, Circle& b, float distance)
+void collisionUpdate(Circle& a, Circle& b)
 {
-    filament::math::float2 colVec = { b.mCenter.x - a.mCenter.x, b.mCenter.y - a.mCenter.y };
-    filament::math::float2 colDir = { colVec.x / distance, colVec.y / distance };
-    filament::math::float2 relativeVelocity = { a.mVelocity.x - b.mVelocity.x, a.mVelocity.y - b.mVelocity.y };
-    
-    float dotProduct = relativeVelocity.x * colDir.x + relativeVelocity.y * colDir.y;
-    if (dotProduct > 0) return;
+    filament::math::float2 origin = { a.mVelocity.x, a.mVelocity.y };
+    filament::math::float2 colDir = { b.mCenter.x - a.mCenter.x, b.mCenter.y - a.mCenter.y };
+    filament::math::float2 normal = { colDir.x / dist(a.mCenter, b.mCenter), colDir.y / dist(a.mCenter, b.mCenter)};
+    filament::math::float2 relativeVelocity = { b.mVelocity.x - a.mVelocity.x, b.mVelocity.y - a.mVelocity.y };
+    float dotProduct = (relativeVelocity.x * normal.x) + (relativeVelocity.y * normal.y);
+    if (dotProduct >= 0) return;
 
-    float j = -(1 + 0.8f) * dotProduct;
-    j /= (1 / a.mMass + 1 / b.mMass);
+    a.mVelocity = { a.mVelocity.x + colDir.x * normal.x, a.mVelocity.y + colDir.y * normal.y };
+    b.mVelocity = { b.mVelocity.x + colDir.x * normal.x, b.mVelocity.y + colDir.y * normal.y };
 
-    filament::math::float2 impulse = { j * colDir.x, j * colDir.y };
-
-    float maxSpeed = 5.0f;
-
-    a.mVelocity.x = filament::math::clamp(a.mVelocity.x - ( 1 / a.mMass * impulse.x ), -maxSpeed, maxSpeed);
-    a.mVelocity.y = filament::math::clamp(a.mVelocity.y - ( 1 / a.mMass * impulse.y ), -maxSpeed, maxSpeed);
-
-    b.mVelocity.x = filament::math::clamp(b.mVelocity.x + ( 1 / b.mMass * impulse.x ), -maxSpeed, maxSpeed);
-    b.mVelocity.y = filament::math::clamp(b.mVelocity.y + ( 1 / b.mMass * impulse.y ), -maxSpeed, maxSpeed);
+    float friction = 0.2f;
+    a.mVelocity = { a.mVelocity.x * friction, a.mVelocity.y * friction };
+    b.mVelocity = { b.mVelocity.x * friction, b.mVelocity.y * friction };
 }
 
 Circle CreateCircle(App& sApp)
@@ -119,14 +116,17 @@ Circle CreateCircle(App& sApp)
     Circle sCircle;
     bool   isCollision;
 
+    sCircle.mRadius   = randf(1.5f, 3.5f);
+    sCircle.mVelocity = { 
+        randf(-3.0f, 3.0f),
+        randf(-3.0f, 3.0f)
+    };
+
     do {
-        isCollision = false;
-        sCircle.mRadius   = randf(1.5f, 3.5f);
-        sCircle.mMass     = sCircle.mRadius * sCircle.mRadius;
-        sCircle.mColor    = (uint32_t)(randf() * (rand() % 0xFFFFFFFF));
-        sCircle.mCenter   = { 
-            randf(-10.0f, 10.0f), 
-            randf(-10.0f, 10.0f) 
+        isCollision     = false;
+        sCircle.mCenter = { 
+            randf(-18.0f, 18.0f), 
+            randf(-18.0f, 18.0f) 
         };
 
         for (int i = 0; i < sApp.mCurrentCount; ++i)
@@ -139,38 +139,26 @@ Circle CreateCircle(App& sApp)
         }
     } while (isCollision);
 
-    sCircle.mVelocity = { 
-        randf(-1.5f, 1.5f) * sCircle.mMass,
-        randf(-1.5f, 1.5f) * sCircle.mMass
-    };
-
-    sCircle.mVertices[0].mPos.x = sCircle.mCenter.x;
-    sCircle.mVertices[0].mPos.y = sCircle.mCenter.y;
-    sCircle.mVertices[0].mColor = sCircle.mColor;
-
-    for (int i = 0; i < gSegments; ++i) 
-    {
-        float theta = (2.0f * M_PI * float(i)) / float(gSegments);
-        sCircle.mVertices[i + 1].mPos.x = sCircle.mCenter.x + sCircle.mRadius * cosf(theta);
-        sCircle.mVertices[i + 1].mPos.y = sCircle.mCenter.y + sCircle.mRadius * sinf(theta);
-        sCircle.mVertices[i + 1].mColor = sCircle.mColor;
-
-        sCircle.mIndicies[i * 3]     = 0;
-        sCircle.mIndicies[i * 3 + 1] = i + 1;
-        sCircle.mIndicies[i * 3 + 2] = (i + 1) % gSegments + 1;
-    }
-
     sApp.mCurrentCount++;
     
     return sCircle;
 }
  
-void Init(App& aApp)
+void initBuffers()
 {
-    aApp.mCurrentCount = 0;
-    for (int i = 0; i < gCircleCount; ++i)
-    {   
-        aApp.mRenderableArr[i].mCircle = CreateCircle(aApp);
+    gVertices[0].mPos   = { 0, 0 };
+    gVertices[0].mColor = (uint32_t)(randf() * (rand() % 0xFFFFFFFF));
+
+    for (int i = 0; i < gSegments; ++i)
+    {
+        float theta = (2.0f * M_PI * float(i)) / float(gSegments);
+        gVertices[i + 1].mPos.x = cosf(theta);
+        gVertices[i + 1].mPos.y = sinf(theta);
+        gVertices[i + 1].mColor = 0xFFFFFFFF;
+
+        gIndicies[i * 3] = 0;
+        gIndicies[i * 3 + 1] = i + 1;
+        gIndicies[i * 3 + 2] = (i + 1) % gSegments + 1;
     }
 }
 
@@ -234,34 +222,29 @@ int main(int argc, char** argv) {
 
     srand(time(NULL));
 
-    Init(sApp);
+    initBuffers();
     
     auto setup = [&sApp](Engine* engine, View* view, Scene* scene) {
         sApp.mSkybox = Skybox::Builder().color({0.1, 0.125, 0.25, 1.0}).build(*engine);
         scene->setSkybox(sApp.mSkybox);
-        
+        auto& tcm = engine->getTransformManager();
         for (int i = 0; i < gCircleCount; ++i)
         {
-            sApp.mRenderableArr[i].mVertexBuffer = VertexBuffer::Builder()
-                                                  .vertexCount(gSegments + 1)
-                                                  .bufferCount(1)
-                                                  .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
-                                                  .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
-                                                  .normalized(VertexAttribute::COLOR)
-                                                  .build(*engine);
+            sApp.mVertexBuffer = VertexBuffer::Builder()
+                                .vertexCount(gSegments + 1)
+                                .bufferCount(1)
+                                .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+                                .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+                                .build(*engine);
 
-            sApp.mRenderableArr[i].mVertexBuffer->setBufferAt(*engine, 0, VertexBuffer::BufferDescriptor(
-                                                              sApp.mRenderableArr[i].mCircle.mVertices,
-                                                              sizeof(Vertex) * (gSegments + 1), nullptr));
+            sApp.mVertexBuffer->setBufferAt(*engine, 0, VertexBuffer::BufferDescriptor(gVertices, sizeof(Vertex) * (gSegments + 1), nullptr));
 
-            sApp.mRenderableArr[i].mIndexBuffer = IndexBuffer::Builder()
-                                                 .indexCount(gSegments * 3)
-                                                 .bufferType(IndexBuffer::IndexType::USHORT)
-                                                 .build(*engine);
+            sApp.mIndexBuffer = IndexBuffer::Builder()
+                               .indexCount(gSegments * 3)
+                               .bufferType(IndexBuffer::IndexType::USHORT)
+                               .build(*engine);
 
-            sApp.mRenderableArr[i].mIndexBuffer->setBuffer(*engine, IndexBuffer::BufferDescriptor(
-                                                           sApp.mRenderableArr[i].mCircle.mIndicies,
-                                                           sizeof(uint16_t) * gSegments * 3, nullptr));
+            sApp.mIndexBuffer->setBuffer(*engine, IndexBuffer::BufferDescriptor(gIndicies, sizeof(uint16_t) * gSegments * 3, nullptr));
 
             sApp.mRenderableArr[i].mMaterial = Material::Builder()
                                               .package(RESOURCES_BAKEDCOLOR_DATA, RESOURCES_BAKEDCOLOR_SIZE)
@@ -269,15 +252,18 @@ int main(int argc, char** argv) {
 
             sApp.mRenderableArr[i].mRenderable = EntityManager::get().create();
 
-            RenderableManager::Builder(1).boundingBox({{ -1, -1, -1 }, { 1, 1, 1 }})
-                                         .material(0, sApp.mRenderableArr[i].mMaterial->getDefaultInstance())
-                                         .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, sApp.mRenderableArr[i].mVertexBuffer, sApp.mRenderableArr[i].mIndexBuffer, 0, gSegments * 3)
+            RenderableManager::Builder(1).material(0, sApp.mRenderableArr[i].mMaterial->getDefaultInstance())
+                                         .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, sApp.mVertexBuffer, sApp.mIndexBuffer, 0, gSegments * 3)
                                          .culling(false)
                                          .receiveShadows(false)
                                          .castShadows(false)
                                          .build(*engine, sApp.mRenderableArr[i].mRenderable);
-
+            
             scene->addEntity(sApp.mRenderableArr[i].mRenderable);
+
+            Circle sCircle = CreateCircle(sApp);
+            sApp.mRenderableArr[i].mCircle = sCircle;
+            tcm.setTransform(tcm.getInstance(sApp.mRenderableArr[i].mRenderable), filament::math::mat4::scaling(sApp.mRenderableArr[i].mCircle.mRadius));
         }
 
         sApp.mCameraObj = utils::EntityManager::get().create();
@@ -287,49 +273,88 @@ int main(int argc, char** argv) {
 
     auto cleanup = [&sApp](Engine* engine, View*, Scene*) {
         engine->destroy(sApp.mSkybox);
-        
+
         for (int i = 0; i < gCircleCount; ++i)
         {
             engine->destroy(sApp.mRenderableArr[i].mRenderable);
             engine->destroy(sApp.mRenderableArr[i].mMaterial);
-            engine->destroy(sApp.mRenderableArr[i].mVertexBuffer);
-            engine->destroy(sApp.mRenderableArr[i].mIndexBuffer);
         }
-        
+
+        engine->destroy(sApp.mVertexBuffer);
+        engine->destroy(sApp.mIndexBuffer);
         engine->destroyCameraComponent(sApp.mCameraObj);
         utils::EntityManager::get().destroy(sApp.mCameraObj);
     };
 
     FilamentApp::get().animate([&sApp](Engine* engine, View* view, double now) {
+        double deltaTime = now - gLastTime;
+        gLastTime = now;
+
         constexpr float ZOOM = 20.0f;
         const uint32_t w = view->getViewport().width;
         const uint32_t h = view->getViewport().height;
         const float aspect = (float) w / h;
-        sApp.mCamera->setProjection(Camera::Projection::ORTHO,
-            -aspect * ZOOM, aspect * ZOOM,
-            -ZOOM, ZOOM, 0, 1);
+        // left, right, bottom, top
+        filament::math::float4 wall = { -aspect * ZOOM, aspect * ZOOM, -ZOOM, ZOOM };
+        sApp.mCamera->setProjection(Camera::Projection::ORTHO, wall.x, wall.y, wall.z, wall.w, 0, 1);
 
         auto& tcm = engine->getTransformManager();
         for (int i = 0; i < gCircleCount; ++i)
         {
-            sApp.mRenderableArr[i].mCircle.mCenter.x += sApp.mRenderableArr[i].mCircle.mVelocity.x / gSpeed;
-            sApp.mRenderableArr[i].mCircle.mCenter.y += sApp.mRenderableArr[i].mCircle.mVelocity.y / gSpeed;
+            Circle& target = sApp.mRenderableArr[i].mCircle;
+            target.mCenter.x += (target.mVelocity.x * gSpeed * deltaTime);
+            target.mCenter.y += (target.mVelocity.y * gSpeed * deltaTime);
 
             tcm.setTransform(tcm.getInstance(sApp.mRenderableArr[i].mRenderable), 
-                             filament::math::mat4f::translation(filament::math::float3(
-                                sApp.mRenderableArr[i].mCircle.mCenter.x,
-                                sApp.mRenderableArr[i].mCircle.mCenter.y, 0)));
-        }
+                             filament::math::mat4f::translation(
+                                filament::math::float3(
+                                target.mCenter.x,
+                                target.mCenter.y, 0)));
 
-        for (int i = 0; i < gCircleCount; ++i)
-        {
+            // left
+            if (abs(wall.x - target.mCenter.x) <= target.mRadius || target.mCenter.x < wall.x)
+            {
+                target.mCenter.x += target.mRadius - target.mCenter.x + wall.x;
+                target.mVelocity.x = -target.mVelocity.x;
+            }
+            // right
+            if (abs(wall.y - target.mCenter.x) <= target.mRadius || target.mCenter.x > wall.y)
+            {
+                target.mCenter.x -= target.mRadius + target.mCenter.x - wall.y;
+                target.mVelocity.x = -target.mVelocity.x;
+            }
+            // bottom
+            if (abs(wall.z - target.mCenter.y) <= target.mRadius || target.mCenter.y < wall.z)
+            {
+                target.mCenter.y += target.mRadius - target.mCenter.y + wall.z;
+                target.mVelocity.y = -target.mVelocity.y;
+            }
+            // up
+            if (abs(wall.w - target.mCenter.y) <= target.mRadius || target.mCenter.y > wall.w)
+            {
+                target.mCenter.y -= target.mRadius + target.mCenter.y - wall.w;
+                target.mVelocity.y = -target.mVelocity.y;
+            }
+
             for (int j = i + 1; j < gCircleCount; ++j)
             {
-                float distance = dist(sApp.mRenderableArr[i].mCircle.mCenter,
-                                      sApp.mRenderableArr[j].mCircle.mCenter);
-                if (distance <= sApp.mRenderableArr[i].mCircle.mRadius + sApp.mRenderableArr[j].mCircle.mRadius)
+                Circle& other = sApp.mRenderableArr[j].mCircle;
+                float distance = dist(target.mCenter, other.mCenter);
+                float diameter = target.mRadius + other.mRadius;
+                float diff     = diameter - distance;
+                if (distance <= diameter)
                 {
-                    Collision(sApp.mRenderableArr[i].mCircle, sApp.mRenderableArr[j].mCircle, distance);
+                    filament::math::float2 firstImpulse = { 
+                        diff / distance * (target.mCenter.x - other.mCenter.x),
+                        diff / distance * (target.mCenter.y - other.mCenter.y),  
+                    };
+                    target.mCenter.x += (firstImpulse.x / 2);
+                    target.mCenter.y += (firstImpulse.y / 2);
+
+                    other.mCenter.x -= (firstImpulse.x / 2);
+                    other.mCenter.y -= (firstImpulse.y / 2);
+
+                    collisionUpdate(target, other);
                 }
             }
         }
